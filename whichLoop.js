7153eb6d015bad3befa3c1cb847f74f250207352
aes89 <adrienne.e.smith@outlook.com>
2020-10-04 19:42:54 +1100

    Loop Completed

---
+++ whichLoop.js
@@ -0,0 +1,57 @@
+function whatDoingToday(day){
+    if (day == "Monday") {
+        return("Learning at Coder Academy")
+    } else if (day == "Tuesday"){
+        return("Learning at Coder Academy")
+    } else if (day == "Wednesday"){
+        return("Learning at Coder Academy")
+    } else if (day == "Thursday"){
+        return("Reviewing lessons from the week")
+    } else if (day == "Friday"){
+        return("Reviewing lessons from the week")
+    } else if (day == "Saturday"){
+        return("It is the weekend!")
+    } else if (day == "Sunday"){
+        return("It is the weekend!")
+    } else {
+        return("That is not a valid day")
+    }   
+}
+// var d = new Date();
+// var day = d.getDay(); 
+
+// function whatDoingToday(day) {
+//     switch (day) {
+//         // case "Monday" || "Tuesday" || "Wednesday":
+//         case 1:
+//             console.log("Learning at Coder Academy");
+//             break;
+//         case 2:
+//             console.log("Learning at Coder Academy");
+//             break;
+//         case 3:
+//             console.log("Learning at Coder Academy");
+//             break;
+//         // case "Thursday" || "Friday":
+//         case 4:
+//             console.log("Reviewing lessons from the week");
+//             break;
+//         case 5:
+//             console.log("Reviewing lessons from the week");
+//             break;
+//         // case "Saturday" || "Sunday" :
+//         case 6:
+//             console.log("It is the weekend!");
+//             break;
+//         case 0:
+//             console.log("It is the weekend!");
+//             break;
+//         default:
+//             console.log("That is not a valid day")
+//             break;
+//     }
+// }
+// What day is it today?
+console.log(whatDoingToday("Tuesday"))
+
+// the tests are dumb. Or I am dumb. But I think it works.
